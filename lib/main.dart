d195cab70c07633370e59dae74550a2c36b79c84
Ricardo Herrera <72674229+ricardoherreral@users.noreply.github.com>
2023-05-23 11:39:29 -0600

    Se quitaron los warnings de main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,9 @@
 import 'package:calculadora/pages/home_page.dart';
-import 'package:calculadora/theme/theme_constants.dart';
-import 'package:calculadora/theme/theme_manager.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const MyApp());
 }
-
-ThemeManager _themeManager = ThemeManager();
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
